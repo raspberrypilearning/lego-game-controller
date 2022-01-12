b1e48d783f40b6e67f6bdae9c386f98eb293a1be
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-01-12 11:54:02 +0000

    New translations two_player_basic.py (German)

--- de-DE/solutions/two_player_basic.py
+++ de-DE/solutions/two_player_basic.py
@@ -2,14 +2,14 @@ from turtle import *
 from time import sleep
 from buildhat import Motor
 
-motor_left = Motor('A')
-motor_right = Motor('B')
+motor_links = Motor('A')
+motor_rechts = Motor('B')
 
-game_area = Screen()
-game_area.title('PONG')
-game_area.bgcolor('black')
-game_area.tracer(0)
-game_area.setworldcoordinates(-200,-170,200,170)
+spielflaeche = Screen()
+spielflaeche.title('PONG')
+spielflaeche.bgcolor('black')
+spielflaeche.tracer(0)
+spielflaeche.setworldcoordinates(-200,-170,200,170)
 
 ball = Turtle()
 ball.color('white')
@@ -17,42 +17,41 @@ ball.shape('circle')
 ball.penup()
 ball.setpos(0,0)
 
-paddle_left = Turtle()
-paddle_left.color('green')
-paddle_left.shape("square")
-paddle_left.shapesize(4,1,1)
-paddle_left.penup()
-paddle_left.setpos(-190,0)
+paddle_links = Turtle()
+schlaeger.links.color('green')
+schlaeger.links.shape("square")
+schlaeger.links.shapesize(4,1,1)
+schlaeger.links.penup()
+schlaeger.links.setpos(-190,0)
 
-paddle_right = Turtle()
-paddle_right.color('blue')
-paddle_right.shape("square")
-paddle_right.shapesize(4,1,1)
-paddle_right.penup()
-paddle_right.setpos(190,0)
+schlaeger_rechts = Turtle()
+schlaeger_rechts.color('blue')
+schlaeger_rechts.shape("square")
+schlaeger_rechts.shapesize(4,1,1)
+schlaeger_rechts.penup()
+schlaeger_rechts.setpos(190,0)
 
 ball.speed_x = 0.4
 ball.speed_y = 0.4
 
-pos_left = 0
-pos_right = 0
+pos_links = 0
+pos_rechts = 0
+
+def drehung_links(motor_speed, motor_rpos, motor_apos):
+    global pos_links
+    pos_links = motor_apos
 
 
-def moved_left(motor_speed, motor_rpos, motor_apos):
-    global pos_left
-    pos_left = motor_apos
+def drehung_rechts(motor_speed, motor_rpos, motor_apos):
+    global pos_rechts
+    pos_rechts = motor_apos
 
 
-def moved_right(motor_speed, motor_rpos, motor_apos):
-    global pos_right
-    pos_right = motor_apos
-
-
-motor_left.when_rotated = moved_left
-motor_right.when_rotated = moved_right
+motor_links.when_rotated = drehung_links
+motor_rechts.when_rotated = drehung_rechts
 
 while True:
-    game_area.update()
+    spielflaeche.update()
     ball.setx(ball.xcor() + ball.speed_x)
     ball.sety(ball.ycor() + ball.speed_y)
     if ball.ycor() > 160:
@@ -63,15 +62,15 @@ while True:
         ball.showturtle()
     if ball.ycor() < -160:
         ball.speed_y *= -1
-    paddle_left.sety(pos_left)
-    paddle_right.sety(pos_right)
-    if (ball.xcor() < -180 and ball.xcor() > -190) and (ball.ycor() < paddle_left.ycor() + 20 and ball.ycor() > paddle_left.ycor() - 20):
+    schlaeger_links.sety(pos_links)
+    schlaeger_rechts.sety(pos_rechts)
+    if (ball.xcor() < -180 and ball.xcor() > -190) and (ball.ycor() < schlaeger_links.ycor() + 20 and ball.ycor() > schlaeger_links.ycor() - 20):
         ball.setx(-180)
         ball.speed_x *= -1
-    if (ball.xcor() > 180 and ball.xcor() < 190) and (ball.ycor() < paddle_right.ycor() + 20 and ball.ycor() > paddle_right.ycor() - 20):
+    if (ball.xcor() > 180 and ball.xcor() < 190) and (ball.ycor() < schlaeger_rechts.ycor() + 20 and ball.ycor() > schlaeger_rechts.ycor() - 20):
         ball.setx(180)
         ball.speed_x *= -1
-    if ball.xcor() < -195: # left
+    if ball.xcor() < -195: # linker Rand
         ball.hideturtle()
         ball.goto(0,0)
         ball.showturtle()
