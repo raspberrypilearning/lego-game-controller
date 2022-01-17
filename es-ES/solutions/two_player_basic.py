628906d3de620b832ff609fc36f3d22f54d0dbb1
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-01-17 14:50:13 +0000

    New translations two_player_basic.py (Spanish)

--- es-ES/solutions/two_player_basic.py
+++ es-ES/solutions/two_player_basic.py
@@ -2,75 +2,75 @@ from turtle import *
 from time import sleep
 from buildhat import Motor
 
-motor_left = Motor('A')
-motor_right = Motor('B')
+motor_izquierda = Motor ('A')
+motor_derecha = Motor ('B')
 
-game_area = Screen()
-game_area.title('PONG')
-game_area.bgcolor('black')
-game_area.tracer(0)
-game_area.setworldcoordinates(-200,-170,200,170)
+juego_area = Screen()
+juego_area.title('PONG')
+juego_area.bgcolor('black')
+juego_area.tracer(0)
+juego_area.setworldcoordinates(-200,-170,200,170)
 
-ball = Turtle()
-ball.color('white')
-ball.shape('circle')
-ball.penup()
-ball.setpos(0,0)
+pelota = Turtle()
+pelota.color('white')
+pelota.shape('circle')
+pelota.penup()
+pelota.setpos(0,0)
 
-paddle_left = Turtle()
-paddle_left.color('green')
-paddle_left.shape("square")
-paddle_left.shapesize(4,1,1)
-paddle_left.penup()
-paddle_left.setpos(-190,0)
+paleta_izquierda = Turtle()
+paleta_izquierda.color('green')
+paleta_izquierda.shape("square")
+paleta_izquierda.shapesize(4,1,1)
+paleta_izquierda.penup()
+paleta_izquierda.setpos(-190,0)
 
-paddle_right = Turtle()
-paddle_right.color('blue')
-paddle_right.shape("square")
-paddle_right.shapesize(4,1,1)
-paddle_right.penup()
-paddle_right.setpos(190,0)
+paleta_derecha = Turtle()
+paleta_derecha.color('blue')
+paleta_derecha.shape("square")
+paleta_derecha.shapesize(4,1,1)
+paleta_derecha.penup()
+paleta_derecha.setpos(190,0)
 
-ball.speed_x = 0.4
-ball.speed_y = 0.4
+pelota.speed_x = 0.4
+pelota.speed_y = 0.4
 
-pos_left = 0
-pos_right = 0
+pos_izquierda = 0
+pos_derecha = 0
 
-def moved_left(motor_speed, motor_rpos, motor_apos):
-    global pos_left
-    pos_left = motor_apos
-
-
-def moved_right(motor_speed, motor_rpos, motor_apos):
-    global pos_right
-    pos_right = motor_apos
+def movido_izquierda(motor_speed, motor_rpos, motor_apos):
+    global pos_izquierda
+    pos_izquierda = motor_apos
 
 
-motor_left.when_rotated = moved_left
-motor_right.when_rotated = moved_right
+def movido_derecha(motor_speed, motor_rpos, motor_apos):
+    global pos_derecha
+    pos_derecha = motor_apos
+
+
+motor_izquierda.when_rotated = movido_izquierda
+motor_derecha.when_rotated = movido_derecha
 
 while True:
-    game_area.update()
-    ball.setx(ball.xcor() + ball.speed_x)
-    ball.sety(ball.ycor() + ball.speed_y)
-    if ball.ycor() > 160:
-        ball.speed_y *= -1
-    if ball.xcor() > 195:
-        ball.hideturtle()
-        ball.goto(0,0)
-        ball.showturtle()
-    if ball.ycor() < -160:
-        ball.speed_y *= -1
-    paddle_left.sety(pos_left)
-    paddle_right.sety(pos_right)
-    if (ball.xcor() < -180 and ball.xcor() > -190) and (ball.ycor() < paddle_left.ycor() + 20 and ball.ycor() > paddle_left.ycor() - 20):
-        ball.setx(-180)
-        ball.speed_x *= -1
-    if (ball.xcor() > 180 and ball.xcor() < 190) and (ball.ycor() < paddle_right.ycor() + 20 and ball.ycor() > paddle_right.ycor() - 20):
-        ball.setx(180)
-        ball.speed_x *= -1
-    if ball.xcor() < -195: # left
-        ball.hideturtle()
-        ball.goto(0,0)
-        ball.showturtle()
+    juego_area.update()
+    pelota.setx(pelota.xcor() + pelota.speed_x)
+    pelota.sety(pelota.ycor() + pelota.speed_y)
+    if pelota.ycor() > 160:
+        pelota.speed_y *= -1
+    if pelota.xcor() > 195:
+        pelota.hideturtle()
+        pelota.goto(0,0)
+        pelota.showturtle()
+    if pelota.ycor() < -160:
+        pelota.speed_y *= -1
+    paleta_izquierda.sety(pos_izquierda)
+    paleta_derecha.sety(pos_derecha)
+    if (pelota.xcor() < -180 and pelota.xcor() > -190) and (pelota.ycor() < paleta_izquierda.ycor() + 20 and pelota.ycor() > paleta_izquierda.ycor() - 20):
+        pelota.setx(-180)
+        pelota.speed_x *= -1
+    if (pelota.xcor() > 180 and pelota.xcor() < 190) and (pelota.ycor() < paleta_derecha.ycor() + 20 and pelota.ycor() > paleta_derecha.ycor() - 20):
+        pelota.setx(180)
+        pelota.speed_x *= -1
+    if pelota.xcor() < -195: # izquierda
+        pelota.hideturtle()
+        pelota.goto(0,0)
+        pelota.showturtle()
